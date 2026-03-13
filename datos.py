16a4a50af33b4ef6a7c4138d6ea9f624c653adc5
Administrador <mpv3108@gmail.com>
2026-03-13 10:18:06 +0100

    Creo las clases y pongo para que se muestre al comprar un producto.

--- datos.py
+++ datos.py
@@ -12,3 +12,18 @@ class Producto(ABC):
     @precio.setter
     def precio(self, valor):
         self.__precio = round(float(valor), 1) if valor >= 0 else 0.0
+
+@abstractmethod
+    def __str__(self): pass
+
+class Electronica(Producto):
+    def __str__(self): return f"[{self.id_prod}] {self.nombre} (Electrónica) - {self.precio}€"
+
+class Ropa(Producto):
+    def __str__(self): return f"[{self.id_prod}] {self.nombre} (Ropa) - {self.precio}€"
+
+class Hogar(Producto):
+    def __str__(self): return f"[{self.id_prod}] {self.nombre} (Hogar) - {self.precio}€"
+
+class Deportes(Producto):
+    def __str__(self): return f"[{self.id_prod}] {self.nombre} (Deportes) - {self.precio}€"
