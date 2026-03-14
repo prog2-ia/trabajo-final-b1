c2103078ef634ac75f3f93d7f1dd8752f498bf63
albersagelias <albersagelias@gmail.com>
2026-03-14 15:13:01 +0100

    Tipos de compradores (subclases de personas) y como muestra de resultado de negociación

--- datos.py
+++ datos.py
@@ -1,3 +1,50 @@
+from abc import ABC, abstractmethod
+import random
+
+# --- JERARQUÍA DE VENDEDORES ---
+class Vendedor(ABC):
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.frases_negociao = []
+
+    @abstractmethod
+    def negociar(self, precio_original, oferta_comprador):
+        pass
+
+class Desesperado(Vendedor):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+        self.frases_negociao = ["¡Necesito comer porfa!", "Uff, lo necesito vender ya...", "Acepta esto, es mi última esperanza."]
+
+    def negociar(self, precio_original, oferta_comprador):
+        if oferta_comprador >= precio_original * 0.70:
+            return "ACEPTADA", round(oferta_comprador, 1)
+        return "RECHAZADA", 0
+
+class Normal(Vendedor):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+        self.frases_negociao = ["Creo que el precio es justo.", "Podemos llegar a un acuerdo.", "Ni para ti ni para mí."]
+
+    def negociar(self, precio_original, oferta_comprador):
+        if oferta_comprador >= precio_original * 0.85:
+            return "ACEPTADA", round(oferta_comprador, 1)
+        elif oferta_comprador >= precio_original * 0.75:
+            contra = round(random.uniform(oferta_comprador, precio_original * 0.95), 1)
+            return "CONTRAOFERTA", contra
+        return "RECHAZADA", 0
+
+class Tacanyo(Vendedor):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+        self.frases_negociao = ["¿Te crees que el dinero crece en los árboles?", "Por ese precio no te doy ni las gracias.", "Si no tienes dinero, no compres."]
+
+    def negociar(self, precio_original, oferta_comprador):
+        if oferta_comprador >= precio_original * 0.95:
+            return "ACEPTADA", round(oferta_comprador, 1)
+        contra = round(random.uniform(precio_original * 0.98, precio_original), 1)
+        return "CONTRAOFERTA", contra
+
 class Producto(ABC):
     def __init__(self, id_prod, nombre, precio, vendedor_obj):
         self.id_prod = id_prod
--- interfaz.py
+++ interfaz.py
@@ -13,3 +13,18 @@ def pedir_datos_venta():
     print("Categoria: 1. Electronica | 2. Ropa | 3. Hogar | 4. Deportes")
     c = input("Opcion: ")
     return n, p, v, c
+
+def mostrar_resultado_negociacion(estado, valor, producto):
+
+    frase = random.choice(producto.vendedor.frases_negociao)
+    if estado == "ACEPTADA":
+        print(f"\n¡TRATO HECHO! {producto.vendedor.nombre} dice: '{frase}'")
+        print(f"Has comprado {producto.nombre} por {valor}€.")
+
+    elif estado == "CONTRAOFERTA":
+        print(f"\n{producto.vendedor.nombre} dice: '{frase}'")
+        print(f"Me parece poco... ¿Qué tal {valor}€?")
+
+    elif estado == "RECHAZADA":
+        print(f"\n{producto.vendedor.nombre} dice: '{frase}'")
+        print("Oferta rechazada. Intenta subir el precio.")
