2cdf75f5080089a9f22077921cdef7d36d6fde7b
albersagelias <albersagelias@gmail.com>
2026-03-15 18:30:43 +0100

    Arreglado el indent

--- interfaz.py
+++ interfaz.py
@@ -2,7 +2,7 @@ import random
 
 def mostrar_menu():
     print("\n--- Marketplace ---")
-    print("1. Ver catalogo\n 2. Vender producto\n 3. Negociar/Comprar\n 4. Salir")
+    print("1. Ver catalogo\n2. Vender producto\n3. Negociar/Comprar\n4. Salir")
     return input("Selección: ")
 
 def pedir_datos_venta():
