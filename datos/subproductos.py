553d474f60412bd17e2e8d2c94d89b11f3d8b191
albersagelias <albersagelias@gmail.com>
2026-04-24 10:14:44 +0200

    Añadido parte de type hinting a subproductos

--- datos/subproductos.py
+++ datos/subproductos.py
@@ -1,13 +1,17 @@
 from datos.producto import Producto
 
 class Electronica(Producto): #Clases derivadas de productos y como se muestran.
-    def __str__(self): return f"[{self.id_prod}] {self.nombre} (Electrónica) - {self.precio}€"
+    def __str__(self) -> str:
+        return f"[{self.id_prod}] {self.nombre} (Electrónica) - {self.precio}€"
 
 class Ropa(Producto):
-    def __str__(self): return f"[{self.id_prod}] {self.nombre} (Ropa) - {self.precio}€"
+    def __str__(self) -> str:
+        return f"[{self.id_prod}] {self.nombre} (Ropa) - {self.precio}€"
 
 class Hogar(Producto):
-    def __str__(self): return f"[{self.id_prod}] {self.nombre} (Hogar) - {self.precio}€"
+    def __str__(self) -> str:
+        return f"[{self.id_prod}] {self.nombre} (Hogar) - {self.precio}€"
 
 class Deportes(Producto):
-    def __str__(self): return f"[{self.id_prod}] {self.nombre} (Deportes) - {self.precio}€"
+    def __str__(self) -> str:
+        return f"[{self  .id_prod}] {self.nombre} (Deportes) - {self.precio}€"
