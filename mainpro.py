7a713871f878599eb31e4776a9a91b9fabe77391
albersagelias <albersagelias@gmail.com>
2026-05-08 10:12:43 +0200

    Arreglado problema de identación

--- mainpro.py
+++ mainpro.py
@@ -47,57 +47,40 @@ def app() -> None:
                     try:
 
                         id_p_str = input("ID del producto: ")
-
                         # Intentamos convertir a número
-
                         id_p = int(id_p_str)
-
                         # Si llega aquí, es que es un número. Buscamos el producto.
-
                         prod = mercado.buscar_producto(id_p)
-
                         negociando: bool = True
 
                         while negociando:
-
                             try:
-
                                 oferta_str = input(f"Oferta por {prod.nombre} (0 para cancelar): ")
-
                                 oferta = float(oferta_str)
 
                                 if oferta < 0:
                                     print("\n[!] ¿Me estás vacilando? Introduce una mejor oferta...")
-
                                     continue
 
                                 if oferta == 0: break
-
                                 res, val = prod.vendedor.negociar(prod.precio, oferta)
-
                                 interfaz.mostrar_resultado_negociacion(res, val, prod)
 
                                 if res == "ACEPTADA":
                                     mercado.comprar_final(id_p)
-
                                     negociando = False
 
 
                             except ValueError:
-
                                 # Este error salta si pones letras en la OFERTA
-
                                 print("[!] Error: Por favor, introduce un número válido para la oferta.")
 
 
                     except ValueError:
-
                         # Este error salta si pones letras en el ID de la compra
-
                         print("\n[!] Error: El ID del producto debe ser un número válido.")
 
                     except ProductoNoEncontradoError as e:
-
                         print(f"\n[ERROR] {e}")
 
 
@@ -105,11 +88,8 @@ def app() -> None:
                 elif opc == "4":
 
                     try:
-
                         id_p_str = input("ID del producto a gestionar: ")
-
                         id_p = int(id_p_str)
-
                         prod = mercado.buscar_producto(id_p)
 
                         # --- VALIDACIÓN DE DUEÑO ---
@@ -118,7 +98,6 @@ def app() -> None:
 
                         if vendedor_auth.lower() != prod.vendedor.nombre.lower():
                             print(f"\n[!] ACCESO DENEGADO: Solo {prod.vendedor.nombre} puede gestionar este producto.")
-
                             continue  # Volver al menú principal
 
                         # Si el nombre coincide, mostramos el submenú
@@ -126,61 +105,45 @@ def app() -> None:
                         sub_opc = interfaz.mostrar_submenu_gestion()
 
                         if sub_opc == "1":
-
                             print(f"\nModificando: {prod.nombre} (Vendedor: {prod.vendedor.nombre})")
-
                             nuevo_nombre = input("Nuevo nombre (deja vacío para no cambiar): ")
-
                             nuevo_precio_str = input("Nuevo precio (deja vacío para no cambiar): ")
 
                             if nuevo_nombre:
                                 prod.nombre = nuevo_nombre
 
                             if nuevo_precio_str:
-
                                 try:
 
                                     p_float = float(nuevo_precio_str)
-
                                     # Esto disparará el setter de Producto que ya tiene su propia excepción
-
                                     prod.precio = p_float
-
                                     print("[*] Precio actualizado.")
 
                                 except (ValueError, ErrorValidacionPrecio) as e:
-
                                     print(f"[!] Error al cambiar precio: {e}")
-
                             print("\n[OK] Características actualizadas con éxito.")
 
 
                         elif sub_opc == "2":
-
                             confirmar = input(f"¿Seguro que quieres eliminar '{prod.nombre}'? (s/n): ")
 
                             if confirmar.lower() == 's':
-
                                 mercado.comprar_final(id_p)
-
                                 print("\n[OK] Producto eliminado correctamente.")
 
                             else:
-
                                 print("\nEliminación cancelada.")
 
 
                         elif sub_opc == "3":
-
                             continue
 
 
                     except ValueError:
-
                         print("\n[!] Error: El ID debe ser un número válido.")
 
                     except ProductoNoEncontradoError as e:
-
                         print(f"\n[ERROR] {e}")
 
                 elif opc == "5":  # Ahora Salir es la opción 5
