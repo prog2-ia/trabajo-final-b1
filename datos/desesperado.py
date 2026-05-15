849f5b519ad94a8b4de94c43481f50e4f6317887
Administrador <mpv3108@gmail.com>
2026-05-15 10:30:36 +0200

    Soluciono problema de inventario.

--- datos/desesperado.py
+++ datos/desesperado.py
@@ -1,11 +1,11 @@
 from datos.vendedor import Vendedor
 
-class Desesperado(Vendedor): #Subclase 1º de vendedores
+class Desesperado(Vendedor): #Subclase 1º de vendedores con mayor probabilidad de vender
     def __init__(self, nombre):
         super().__init__(nombre)
         self.frases_negociao = ["¡Necesito comer porfa!", "Uff, lo necesito vender ya...", "Acepta esto, es mi última esperanza. :("]
 
-    def negociar(self, precio_original: float, oferta_comprador: float) -> tuple:
+    def negociar(self, precio_original: float, oferta_comprador: float) -> tuple: #Forma de comprar de un desesperado
         if oferta_comprador >= precio_original * 0.70:
             return "ACEPTADA", round(oferta_comprador, 1)
         return "RECHAZADA", 0.0
