2e54f70f8d6fd31b0d229bc48d3d8a74dceb0fae
Administrador <mpv3108@gmail.com>
2026-05-15 10:39:23 +0200

    Comentarios tacanyo.

--- datos/vendedor.py
+++ datos/vendedor.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 
-class Vendedor(ABC):
+class Vendedor(ABC): #Clase padre de todos los tipos de vendedores, especificamos atributos necesarios.
     def __init__(self, nombre: str):
         self.nombre: str = nombre
         self.frases_negociao: list[str] = []
 
-    @abstractmethod
+    @abstractmethod #Clase padre de negociar con sus atributos necesarios.
     def negociar(self, precio_original: float, oferta_comprador: float) -> tuple:
         pass
