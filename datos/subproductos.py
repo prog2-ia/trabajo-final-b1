e264771f557a567912f824c31d15060db9699df2
Administrador <mpv3108@gmail.com>
2026-05-15 10:36:25 +0200

    Comentarios subproductos.

--- datos/subproductos.py
+++ datos/subproductos.py
@@ -4,14 +4,14 @@ class Electronica(Producto): #Clases derivadas de productos y como se muestran.
     def __str__(self) -> str:
         return f"[{self.id_prod}] {self.nombre} (Electrónica) - {self.precio}€"
 
-class Ropa(Producto):
+class Ropa(Producto): #Subclase numero 1
     def __str__(self) -> str:
         return f"[{self.id_prod}] {self.nombre} (Ropa) - {self.precio}€"
 
-class Hogar(Producto):
+class Hogar(Producto): #Subclase numero 2
     def __str__(self) -> str:
         return f"[{self.id_prod}] {self.nombre} (Hogar) - {self.precio}€"
 
-class Deportes(Producto):
+class Deportes(Producto): #Subclase numero 3
     def __str__(self) -> str:
         return f"[{self  .id_prod}] {self.nombre} (Deportes) - {self.precio}€"
