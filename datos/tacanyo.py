56cbaf62de27f64a3f12bd1c3610e87d1a345e5f
Administrador <mpv3108@gmail.com>
2026-05-15 10:37:55 +0200

    Comentarios tacanyo.

--- datos/tacanyo.py
+++ datos/tacanyo.py
@@ -1,19 +1,18 @@
 from datos.vendedor import Vendedor
 import random
 
-
-class Tacanyo(Vendedor):  # Subclase de vendedor.
+class Tacanyo(Vendedor):  # Subclase de vendedor, este es mas dificil que te lo venda
     def __init__(self, nombre: str):  #Tipo simple str
         super().__init__(nombre)
         self.frases_negociao: list[str] = ["¿Te crees que el dinero crece en los árboles?",
                                            "Por ese precio no te doy ni las gracias.",
                                            "Si no tienes dinero, no compres."]
 
-    def negociar(self, precio_original: float,
+    def negociar(self, precio_original: float, #Forma de negociar de un tacanyo.
                  oferta_comprador: float) -> tuple:  #Parámetros float y retorno de tupla
         if oferta_comprador >= precio_original * 0.95:
             return "ACEPTADA", round(oferta_comprador, 1)
 
-        # P04: Variable local tipada como float
+        # P04: Variable local puesta como float
         contra: float = round(random.uniform(precio_original * 0.98, precio_original), 1)
         return "CONTRAOFERTA", contra
